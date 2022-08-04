92a549c1551066ff3ddc5d3b9bb10534652d013d
hainswor@wakehealth.edu <hainswor@wakehealth.edu>
2022-08-04 13:43:18 -0400

    create foo
    We tested adding different function elements.

---
+++ R/foo.R
@@ -0,0 +1,27 @@
+#' A Function to Foo Your Data
+#'
+#' This is a description of my function
+#'
+#' @param x (numeric, length of 1) A vector to foo.
+#'
+#' @return Returns a vector the same length as x with a value of 1 added to it.
+#'   This is another sentence.
+#'
+#' @export
+#'
+#' @details Here are some more details about `x`.
+#'
+#' @references
+#'
+#' @examples
+#'
+#' #Here is a standard way to use the foo function.
+#' foo(1)
+#'
+#' #Here is how you apply foo to a vector.
+#' foo(1:10)
+#'
+foo=function(x){
+  x + 1
+
+}

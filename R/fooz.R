0ba1a4baa2fea4d71fe868735bf4542fd9330015
hainswor@wakehealth.edu <hainswor@wakehealth.edu>
2022-08-05 13:42:29 -0400

    A second example of sending commit to github
    we have downloaded a respository from github, made changes and are now sending them back.

--- R/fooz.R
+++ R/fooz.R
@@ -1,3 +1,17 @@
+#'A function to Add two to x
+#'
+#'This takes a number and then adds two to it.
+#'
+#'@param x (numeric, length 1) a vector to foo
+#'
+#'@return This returns a vector that is the same length of x.
+#'
+#'@export
+#'@details here are more details a lot of details so many details we have to
+#' print on another line
+#'
+
+
 fooz=function(x){
   x + 2
 
